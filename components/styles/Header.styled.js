4adbd67b29ef7bce89bfc50a7ee916566bb8fd33
Mark Jager <markjager13@gmail.com>
2022-04-13 04:32:43 +0200

    adjusted header font, used grid and flex for navbar, added hover effect

--- components/styles/Header.styled.js
+++ components/styles/Header.styled.js
@@ -3,20 +3,43 @@ import styled from 'styled-components';
 export const StyledHeader = styled.header`
     width: 100%;
     padding: 1em 0;
+    margin: 0 auto;
 `;
 
 export const StyledNav = styled.nav`
-    display: flex;
-    justify-content: flex-end;
+    display: grid;
+    grid-template-columns: 1fr 400px;
 `;
 
 export const NavLinks = styled.ul`
+    grid-column: 2/3;
     display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const NavLink = styled.a`
-    padding: 20px;
-    font-size: 1.125rem;
+    position: relative;
+    margin-left: 2em;
+    margin-bottom: 0;
+    font-size: 1.25rem;
     color: #303030;
     cursor: pointer;
+
+    &:before {
+        position: absolute;
+        content: '';
+        display: block;
+        height: 3px;
+        background: black;
+        bottom: -0.25em;
+        left: 0;
+        right: 0;
+        transform: scale(0, 1);
+        transition: transform ease-in-out 250ms;
+    }
+
+    &:hover:before {
+        transform: scale(1, 1);
+    }
 `;
