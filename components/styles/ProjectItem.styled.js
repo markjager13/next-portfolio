8e53bed506a7ed477c4cc8994f4ea6e745d1729f
Mark Jager <markjager13@gmail.com>
2022-04-21 14:35:25 +0200

    added project item navigation

--- components/styles/ProjectItem.styled.js
+++ components/styles/ProjectItem.styled.js
@@ -8,6 +8,17 @@ export const Section = styled.div`
 
     position: relative;
 
+    @media (min-width: 600px) {
+        display: grid;
+        width: min-content;
+        margin: 0 auto;
+        grid-column-gap: 1em;
+        grid-template-areas:
+          "img title"
+          "img subtitle";
+        grid-template-columns: min-content max-content;
+    }
+
 `;
 
 export const Title = styled.h1`
@@ -18,4 +29,51 @@ export const Title = styled.h1`
         display: block;
     }
 
+`;
+
+export const SubTitle = styled.p`
+    margin: 0;
+    font-size: 1.25rem;
+
+    background: #d7f6f6;
+    padding: 0.25em 1em;
+    font-family: sans-serif;
+    margin-bottom: 1em;
+
+    align-self: start;
+    grid-column: -1 / 1;
+    grid-row: 2;
+    text-align: right;
+    position: relative;
+    left: -1.5em;
+    width: calc(100% + 1.5em);
+`;
+
+export const TitleImage = styled.div`
+    grid-area: img;
+    min-width: 250px;
+    position: relative;
+    z-index: 2;
+`;
+
+export const ProjectLinkWrapper = styled.div`
+    text-align: center;
+    padding-bottom: 26px;
+
+    a {
+        padding: 0 1em;
+        display: inline-block;
+        color: #303030;
+        font-size: 1.5rem;
+        text-decoration: none;
+    }
+
+`;
+
+export const NextLink = styled.a`
+  margin-left: 1em;
+`;
+
+export const PrevLink = styled.a`
+  margin-right: 1em;
 `;
--- pages/project_item.js
+++ pages/project_item.js
@@ -1,14 +1,59 @@
 import React from 'react'
 import Link from 'next/link'
-import { Section, Title } from '../components/styles/ProjectItem.styled';
+import Image from 'next/image';
+import GlobalStyles from '../components/styles/Global';
+import { Section, Title, SubTitle, TitleImage, ProjectLinkWrapper, ProjectLink, NextLink, PrevLink } from '../components/styles/ProjectItem.styled';
+import { FaChevronLeft, FaChevronRight, FaGithub, FaRegEye } from 'react-icons/fa';
 
 const project_item = () => {
   return (
-    <Section>
-        <Title>
-            The title <strong>of my project</strong>
-        </Title>
-    </Section>
+    <>
+      <GlobalStyles />
+      <Section>
+          <Title>
+              Gallery <br />
+              Shift Calculator
+          </Title>
+          <SubTitle>
+            React, CSS Modules, Netlify
+          </SubTitle>
+          <TitleImage>
+            <Image 
+              className="project1__img"
+              src="/projectimage01.jpg"
+              alt="project image"
+              width={480}
+              height={480}
+              layout="responsive"
+            />
+          </TitleImage>
+      </Section>
+      <ProjectLinkWrapper>
+        <Link href="#" passHref>
+          <PrevLink>
+            <FaChevronLeft />
+          </PrevLink>
+        </Link>
+        <Link href="#" passHref>
+          <a>
+            <FaGithub />
+            Project GitHub
+          </a>
+        </Link>
+        <span> | </span>
+        <Link href="#" passHref>
+          <a>
+            <FaRegEye />
+            Live Demo
+          </a>
+        </Link>
+        <Link href="#" passHref>
+          <NextLink>
+            <FaChevronRight />
+          </NextLink>
+        </Link>
+      </ProjectLinkWrapper>
+    </>
   )
 }
 
