a1e8586e8d68ca6e5e249cc4d66f6f9902f7dd1f
Mark Jager <markjager13@gmail.com>
2022-11-05 05:31:04 -0400

    Reworded about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,9 +19,9 @@ const About = () => {
             />
         </ProfileImg>
         <AboutText>
-            <p>My interest in programming started when I was planning my transition out of the Army. I got hooked on a few online tutorials and decided to pursue a CS degree.</p>
-            <p>But what really transformed my interest was when I deployed an app to solve a problem at work. It was a simple tool, but I was so thrilled that I was able to solve an actual problem with code.</p>
-            <p>That moment gave me a clear understanding of why I wanted to build software. The process of visualizing a solution, building it, and bringing it to life is very rewarding to me.</p>
+            <p>My interest in programming started when I was planning my transition out of the Army. After reading a few textbooks and learning the basics, I decided to pursue a degree in computer science.</p>
+            <p>It was challenging, but it was a great decision. I learned a great deal about object-oriented programming, data structures, and algorithms.</p>
+            <p>Above all, I learned why I wanted to build software. The process of visualizing a solution, building it, and bringing it to life is very rewarding to me.</p>
         </AboutText>
     </AboutSection>
   )
